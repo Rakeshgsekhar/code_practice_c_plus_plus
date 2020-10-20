2f01404885988811d1ddd7b410b48afa73f3f8c9
Rakeshgsekhar <rakeshgsekhar.fortune@gmail.com>
2020-10-20 07:53:37 +0530

    sortedRotatedArray Search

---
+++ codes/sortedRotatedSearch.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include<string>
+#include <cstring>
+#include<conio.h>
+#include <unordered_map>
+
+using namespace std;
+
+int search(int arr[],int l, int h, int key){
+    if(l>h) return -1;
+    int mid = (l + h) /2;
+    if(arr[mid] == key) return mid;
+
+    if(arr[l]<=arr[mid]){
+        if(key >= arr[l] && key<=arr[mid]) return search(arr,l,mid-1,key);
+
+        return search(arr,mid+1,h,key);
+    }
+
+    if(key >= arr[mid] && key<=arr[h]) return search(arr,mid+1,h,key);
+
+    return search(arr,l,mid-1,key);
+}
+
+int main(){
+    int arr[] = {3,4,5,6,1,2};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int index = search(arr,0,n,1);
+    cout<<index;
+    return 0;
+}
